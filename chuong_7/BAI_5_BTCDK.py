d5dff37e0ae4c806f6049d18caaf746a2e69cd8f
vu_thi_ha <vtha.24174600095@sv.uneti.edu.vn>
2024-10-31 10:45:34 +0700

    bài 5

---
+++ chuong_7/BAI_5_BTCDK.py
@@ -0,0 +1,7 @@
+ky_tu = str(input("nhap mot chu cai : "))
+nguyen_am = "aeiouAEIOU"
+if ky_tu in nguyen_am:
+    print(f" ky tu {ky_tu} la nguyen am")
+else:
+    print(f"ky tu {ky_tu} la phu am ") 
+       
