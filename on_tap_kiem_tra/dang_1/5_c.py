8e663e29528a679623bf2435f8fca27ce5581ed8
vu_thi_ha <vtha.24174600095@sv.uneti.edu.vn>
2025-01-10 21:36:10 +0700

    5c

--- on_tap_kiem_tra/dang_1/5_c.py
+++ on_tap_kiem_tra/dang_1/5_c.py
@@ -4,8 +4,8 @@ if n > 4:
     for i in range(4,n+1):# vòng lặp từ 4 đến n
         a=a+((i+1)/(i-1))
     b = 0     
-    for k in range(1,n+1):
-        b=b+(k/k+1) 
+    for i in range(1,n+1):
+        b=b+(i/i+1) 
     s = a + b  
     print(f"s = {s}")    
 else:
